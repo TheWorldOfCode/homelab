cae045a04f2a727122509c68189ffaea2238b4b8
TheWorldOfCode <dannj75@gmail.com>
2022-07-20 17:20:02 +0200

    Next step

--- pihole/auto_local_dns/auto_local_dns.py
+++ pihole/auto_local_dns/auto_local_dns.py
@@ -2,10 +2,45 @@
 import logging
 import docker
 from time import sleep
+from typing import List
 import signal
 import os
 
 STOP = False
+
+class Domain():
+
+    """ Contains a domain and ip address """
+
+    def __init__(self, name: str, ip: str):
+        """ Create the object 
+
+        @param name The domain name
+        @param ip The ip address
+
+        """
+        self._name = name
+        self._ip = ip
+
+    def set_ip(self, new_ip: str):
+        """ Set a new ip for domain """
+        self._ip = new_ip
+
+    def to_file(self) -> str:
+        """ Return information according to pihole file format """
+        return f"{self._ip} {self._name}"
+
+    def __eq__(self, b) -> bool:
+        """ Compare two domains """
+
+        if self._name != b._name:
+            return False
+
+        if self._ip != self._ip:
+            return False
+
+        return True
+
 
 def stop(signum, frame):
     global STOP
@@ -19,22 +54,43 @@ def main():
     label = os.getenv("LABEL_URL", "pihole.url")
     network = os.getenv("DOCKER_NETWORK", "frontend")
     pihole_list = os.getenv("PIHOLE_DNS_LIST_PATH", "/etc/pihole/custom.list")
-    domain_ext = os.get("DOMAIN_EXTENSION", "home")
+    domain_ext = os.getenv("DOMAIN_EXTENSION", "home")
+    overwrite_domain = os.getenv("OVERWRITE_DOMAIN_IP", "")
 
     print(f"Schedule time: {time}")    
     print(f"Label url: {label}")    
     print(f"Network: {network}")    
     print(f"Pihole list: {pihole_list}")    
     print(f"Domain extension: {domain_ext}")    
+    print(f"Overwrite domain ip: {overwrite_domain}")    
 
     signal.signal(signal.SIGINT, stop)
 
     while not STOP:
-        task(label, network, pihole_list, domain_ext)
+        task(label, network, pihole_list, domain_ext, overwrite_domain)
         sleep(time)
 
+def compare_domains(list_a: List[Domain], list_b: List[Domain]) -> bool:
+    """
+        Compare two list with domains
 
-def task(label, network, pihole_list, domain_ext):
+        @param list_a The first list
+        @param list_b The second list
+        @return True if the lists contains the same domains
+    """
+
+    for a_domain in list_a:
+        for b_domain in list_b:
+
+            if a_domain == b_domain:
+                pass
+            else:
+                return False
+
+    return True
+
+
+def task(label, network, pihole_list, domain_ext, overwrite_domain):
     client = docker.from_env()
 
     containers = []
@@ -44,21 +100,30 @@ def task(label, network, pihole_list, domain_ext):
             ip_addr = con.attrs['NetworkSettings']["Networks"][network]['IPAddress']
 
             if domain.split(".")[-1] == domain_ext:
-                containers.append(f"{ip_addr} {domain}")
+                D = Domain(domain, ip_addr)
+                if overwrite_domain != "":
+                    D.set_ip(overwrite_domain)
+                containers.append(D)
         except:
             pass
 
     if len(containers) != 0:
+        
+        pihole = []
+        with open(pihole_list, "r") as f:
+            for line in f.readlines():
+                ip, domain = line.replace("\n", "").split(" ")
+                pihole.append(Domain(domain, ip))
 
-        with open(pihole_list, "w") as f:
-            f.writelines(containers)
-
-
-
-
-
+        
+        if not compare_domains(pihole, containers):
+            with open(pihole_list, "w") as f:
+                for d in containers:
+                    f.write(f"{d.to_file()}\n")
+            print("UPDATE DNS SERVER")
 
 
 
 if __name__ == "__main__":
+    print("ENTER")
     main()
